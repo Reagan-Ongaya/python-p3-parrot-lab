e09580694e21cd1dc37c4c4dd84ba7514f8c4c3c
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-02 23:12:54 +0300

    Assignment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,7 @@
-def parrot():
-    pass
+def parrot(message="Squawk!"):
+    print(message)
+    return message
+
+default_parrot_message = parrot()  
+custom_parrot_message = parrot("Hello, I'm a parrot!")
+
